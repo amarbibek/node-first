6d8f9322be715964113d8c4a3bfd089014a1cf50
amarbibek <kumar.bee014@gmail.com>
2018-12-25 04:33:16 +0530

    initial changes

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+// to register hbs partials
+hbs.registerPartials(__dirname + "/views/partials");
+// to register helpers
+hbs.registerHelper("currentYear", new Date().getFullYear());
+//to set view engine
+app.set("view engine", "hbs");
+//to use static files like css/images
+app.use(express.static(__dirname + "/public"));
+// to register middleware eg. this is used to log the requests and time
+app.use((req, res, next) => {
+  var data = `${new Date().toString()}- ${req.method} - ${req.url}`;
+  fs.appendFileSync("log.log", data + "\n");
+  //   console.log(``);
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.send("this is test app");
+});
+
+app.get("/about", function(req, res) {
+  // res.send("this is test app");
+  res.render("about.hbs", {
+    title: "about us page",
+    content: "this is the content of about us page"
+    // currentYear: new Date().getFullYear()
+  });
+});
+
+app.get("/contact", function(req, res) {
+  // res.send("this is test app");
+  res.render("contact.hbs", {
+    title: "contact us page",
+    content: "this is the content of about us page"
+  });
+});
+
+app.listen("3000", () => {
+  console.log("app is listening on port 3000");
+});
